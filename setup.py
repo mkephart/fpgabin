97411e8292d34d456401a3a6c38f53e145256421
Miranda Kephart <miranda.kephart@gmail.com>
2015-08-31 15:44:39 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fpgabin',
+  packages = ['fpgabin'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Creates binaries for FPGA upload',
+  author = 'Miranda Kephart',
+  author_email = 'miranda.kephart@gmail.com',
+  url = 'https://github.com/mkephart/fpgabin',
+  download_url = 'https://github.com/mkephart/fpgabin/tarball/src',
+  keywords = [],
+  classifiers = [],
+)
